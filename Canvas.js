64c13488d4d3df2648a3f75a7f4dde38b6d28e4e
Agustina Soto <agustinasoto239@gmail.com>
2025-08-11 20:00:48 -0300

    fix comments

--- Canvas.js
+++ Canvas.js
@@ -1,13 +1,13 @@
 import { drawResultsPoses, drawResultsHands } from "./utilities.js";
 //Canvas es como un "lienzo digital" en una página web donde podes dibujar
 export class Canvas {
-  //obtiene el contexto para dibujar
+  // Obtiene el contexto para dibujar
   constructor() {
     this.canvas = document.querySelector("canvas");
     this.ctx = this.canvas.getContext("2d");
   }
 
-  //toma las dimensiones del canvas
+  // Toma las dimensiones del canvas
   setWidthHeight(width, height) {
     this.canvas.width = width;
     this.canvas.height = height;
@@ -16,17 +16,20 @@ export class Canvas {
   getCtx() {
     return this.ctx;
   }
-  //dibuja las poses detectadas
+
+  // Dibuja las poses detectadas
   drawResultsPoses(poses) {
     // No dibujamos nada aquí, solo queremos las manos
     // drawResultsPoses(this.ctx, poses);
     return;
   }
-//dibuja las manos detectadas
+
+  // Dibuja las manos detectadas
   renderHands(hands, handToPlayer) {
     drawResultsHands(this.ctx, hands, handToPlayer);
   }
-  //Dibuja el frame actual del video de la cámara en el canvas
+
+  // Dibuja el frame actual del video de la cámara en el canvas
   drawCameraFrame(camera) {
     this.ctx.drawImage(
       camera.getVideo(),
